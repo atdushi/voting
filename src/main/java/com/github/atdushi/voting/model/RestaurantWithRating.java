ae250a1960575d8d8abceefa1bf74a121c72a293
atdushi <atdushi@users.noreply.github.com>
2025-01-07 22:28:21 +0300

    restaurant API refactoring

--- src/main/java/com/github/atdushi/voting/model/RestaurantWithRating.java
+++ src/main/java/com/github/atdushi/voting/model/RestaurantWithRating.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.github.atdushi.common.HasId;
 import com.github.atdushi.voting.to.RestaurantTo;
 
-@JsonDeserialize(as = RestaurantTo.class)
+@JsonDeserialize(as = RestaurantTo.class)   // so Jackson will use RestaurantTo for deserialization
 public interface RestaurantWithRating extends HasId {
     String getName();
     Integer getRating();
